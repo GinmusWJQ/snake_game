7d59241fb5ed16c438f4edd26b97c4a562868e47
GinmusWJQ <wjq20010505@163.com>
2023-02-27 22:43:08 +0800

    初步完成绘画地图和蛇

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,40 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(name);
+pub struct SnakeCell(usize);
+#[wasm_bindgen]
+pub struct Snake {
+    body: Vec<SnakeCell>,
+}
+#[wasm_bindgen]
+impl Snake {
+    pub fn new(spawn_index: usize) -> Self {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
+    }
 }
 
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+pub struct World {
+    width: usize,
+    snake: Snake,
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new() -> Self {
+        World {
+            width: 32,
+            snake: Snake::new(10),
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
 }
--- view/src/main.ts
+++ view/src/main.ts
@@ -1,28 +1,38 @@
-import './style.css';
-import typescriptLogo from './typescript.svg';
-import { setupCounter } from './counter';
-import init, { greet } from 'snake_game';
+import init, { World } from 'snake_game';
 
 init().then((_) => {
-  greet('hello');
+  const CELL_SIZE = 20;
+  const world = World.new();
+  const worldWidth = world.width();
+  const canvas = document.getElementById('snake-canvas') as HTMLCanvasElement;
+  const ctx = canvas.getContext('2d')!;
+  canvas.height = CELL_SIZE * worldWidth;
+  canvas.width = CELL_SIZE * worldWidth;
+
+  function drawWorld() {
+    ctx.beginPath();
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
+    }
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
+    }
+    ctx.stroke();
+  }
+
+  function drawSnake() {
+    const snakeIdx = world.snake_head_idx();
+    const col = snakeIdx % worldWidth;
+    const row = Math.floor(snakeIdx / worldWidth);
+
+    ctx.beginPath();
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.stroke();
+  }
+
+  console.log(world.snake_head_idx());
+  drawWorld();
+  drawSnake();
 });
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
