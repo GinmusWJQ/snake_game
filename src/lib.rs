22a5397a3d44df4382289e2a54591878cdcd923c
GinmusWJQ <104190153+GinmusWJQ@users.noreply.github.com>
2023-03-03 17:39:41 +0800

    完成整条蛇的移动

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-enum Direction {
+#[wasm_bindgen]
+pub enum Direction {
     Up,
     Down,
     Right,
@@ -11,6 +12,7 @@ enum Direction {
 }
 
 #[wasm_bindgen]
+#[derive(Clone)]
 pub struct SnakeCell(usize);
 #[wasm_bindgen]
 pub struct Snake {
@@ -19,11 +21,12 @@ pub struct Snake {
 }
 #[wasm_bindgen]
 impl Snake {
-    pub fn new(spawn_index: usize) -> Self {
-        Snake {
-            body: vec![SnakeCell(spawn_index)],
-            direction: Direction::Up,
+    pub fn new(spawn_index: usize, direction: Direction, size: usize) -> Self {
+        let mut body: Vec<_> = vec![];
+        for i in 0..size {
+            body.push(SnakeCell(spawn_index - i))
         }
+        Snake { body, direction }
     }
 }
 
@@ -32,15 +35,17 @@ pub struct World {
     width: usize,
     size: usize,
     snake: Snake,
+    reward_cell: usize,
 }
 
 #[wasm_bindgen]
 impl World {
-    pub fn new(width: usize, snake_idx: usize) -> Self {
+    pub fn new(width: usize, snake_idx: usize, direction: Direction, snake_size: usize) -> Self {
         World {
             width,
             size: width * width,
-            snake: Snake::new(snake_idx),
+            snake: Snake::new(snake_idx, direction, snake_size),
+            reward_cell: 10,
         }
     }
 
@@ -48,44 +53,61 @@ impl World {
         self.width
     }
 
+    pub fn reward_cell(&self) -> usize {
+        self.reward_cell
+    }
+
     /// `snake_head_idx`返回蛇头所处的index
     pub fn snake_head_idx(&self) -> usize {
         self.snake.body[0].0
     }
 
-    /** `update`根据蛇的的前进方向，找出蛇的下一个位置，并将下一个位置更新到自身`self.body[0].0`中
+    /** `step`根据蛇的的前进方向，找出蛇的下一个位置，并将下一个位置更新到自身`self.snake.body`中
      *  #Example
      *  ```
      *  //直接调用即可
-     *  self.update()
+     *  self.step()
      *  ```
      * */
-    pub fn update(&mut self) {
+    pub fn step(&mut self) {
+        let temp = self.snake.body.clone();
+        let next_cell = self.gen_next_snake_cell(&self.snake.direction);
+        self.snake.body[0] = next_cell;
+
+        let len = self.snake.body.len();
+
+        for i in 1..len {
+            self.snake.body[i] = SnakeCell(temp[i - 1].0)
+        }
+    }
+
+    ///生成下一个蛇单元
+    fn gen_next_snake_cell(&self, direction: &Direction) -> SnakeCell {
         let snake_idx = self.snake_head_idx();
-        let (row, col) = self.index_to_cell(snake_idx);
-        let (row, col) = match self.snake.direction {
-            Direction::Right => (row, (snake_idx + 1) % self.width),
-            Direction::Left => (row, (snake_idx - 1) % self.width),
-            Direction::Up => ((row - 1) % self.width, col),
-            Direction::Down => ((row + 1) % self.width, col),
-            _ => (row, col),
-        };
-        let next_idx = self.cell_to_index(row, col);
-        self.set_snake_head(next_idx);
+        let row = snake_idx / self.width;
+        match direction {
+            Direction::Right => SnakeCell((row * self.width) + (snake_idx + 1) % self.width),
+            Direction::Left => SnakeCell((row * self.width) + (snake_idx - 1) % self.width),
+            Direction::Up => SnakeCell((snake_idx - self.width) % self.size),
+            Direction::Down => SnakeCell((snake_idx + self.width) % self.size),
+        }
     }
 
-    /// `set_snake_head`接收index，并将该index更新到自身`self.body[0].0`中
-    fn set_snake_head(&mut self, idx: usize) {
-        self.snake.body[0].0 = idx;
+    /// 获得蛇数组的首地址指针
+    pub fn snake_cells(&self) -> *const SnakeCell {
+        self.snake.body.as_ptr()
     }
 
-    /// 根据蛇在整个数组中所处的index,算出蛇所处的行和列,返回`(row,col)`
-    fn index_to_cell(&self, index: usize) -> (usize, usize) {
-        (index / self.width, index % self.width)
+    /// 获得蛇数组的长度
+    pub fn snake_len(&self) -> usize {
+        self.snake.body.len()
     }
 
-    /// 根据蛇所处的行和列,算出蛇在整个数组中所处的index
-    fn cell_to_index(&self, row: usize, col: usize) -> usize {
-        (row * self.width) + col
+    /// 改变蛇的方向
+    pub fn change_snake_dir(&mut self, direction: Direction) {
+        let next_cell = self.gen_next_snake_cell(&direction);
+        if next_cell.0 != self.snake.body[1].0 {
+            self.snake.direction = direction;
+        }
     }
 }
--- view/src/main.ts
+++ view/src/main.ts
@@ -1,27 +1,43 @@
-import init, { World } from 'snake_game';
+import init, { World, Direction } from 'snake_game';
 
-init().then((_) => {
+init().then((wasm) => {
   const CELL_SIZE = 20;
   const WORLD_WIDTH = 32;
   const WORLD_SIZE = WORLD_WIDTH * WORLD_WIDTH;
   const SNAKE_SPAWN_IDX = Date.now() % WORLD_SIZE;
   const FPS = 10;
-  const world = World.new(WORLD_WIDTH, SNAKE_SPAWN_IDX);
+  let currentDirection: Direction = Direction.Right;
+  let snakeSize: number = 5;
+  const world = World.new(
+    WORLD_WIDTH,
+    SNAKE_SPAWN_IDX,
+    currentDirection,
+    snakeSize
+  );
   const worldWidth = world.width();
   const canvas = document.getElementById('snake-canvas') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d')!;
   canvas.height = CELL_SIZE * worldWidth;
   canvas.width = CELL_SIZE * worldWidth;
 
+  /**添加键盘点击事件 */
   document.addEventListener('keydown', (event) => {
-    if (event.code === 'ArrowLeft' || event.code === 'KeyA') {
-      console.log('change dir to left');
-    } else if (event.code === 'ArrowUp' || event.code === 'KeyW') {
-      console.log('change dir to up');
-    } else if (event.code === 'ArrowRight' || event.code === 'KeyD') {
-      console.log('change dir to right');
-    } else if (event.code === 'ArrowDown' || event.code === 'KeyS') {
-      console.log('change dir to down');
+    const keyMapDirection: Record<string, Direction | undefined> = {
+      ArrowLeft: Direction.Left,
+      KeyA: Direction.Left,
+      ArrowUp: Direction.Up,
+      KeyW: Direction.Up,
+      ArrowRight: Direction.Right,
+      KeyD: Direction.Right,
+      ArrowDown: Direction.Down,
+      KeyS: Direction.Down,
+    };
+    const targetDirection = keyMapDirection[event.code];
+    if (typeof targetDirection !== 'undefined') {
+      if (targetDirection !== currentDirection) {
+        world.change_snake_dir(targetDirection);
+        currentDirection = targetDirection;
+      }
     }
   });
 
@@ -39,11 +55,29 @@ init().then((_) => {
   }
 
   function drawSnake() {
-    const snakeIdx = world.snake_head_idx();
-    const col = snakeIdx % worldWidth;
-    const row = Math.floor(snakeIdx / worldWidth);
+    const snakeCells = new Uint32Array(
+      wasm.memory.buffer,
+      world.snake_cells(),
+      world.snake_len()
+    );
 
+    snakeCells.forEach((cellIdx, i) => {
+      const col = cellIdx % worldWidth;
+      const row = Math.floor(cellIdx / worldWidth);
+
+      ctx.fillStyle = i == 0 ? '#7878bd' : '#000';
+      ctx.beginPath();
+      ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      ctx.stroke();
+    });
+  }
+
+  function drawReward() {
+    const idx = world.reward_cell();
+    const col = idx % worldWidth;
+    const row = Math.floor(idx / worldWidth);
     ctx.beginPath();
+    ctx.fillStyle = '#FF0000';
     ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
     ctx.stroke();
   }
@@ -51,6 +85,7 @@ init().then((_) => {
   function paint() {
     drawWorld();
     drawSnake();
+    drawReward();
   }
 
   function update(timer?: number) {
@@ -60,7 +95,7 @@ init().then((_) => {
     return () => {
       let t = setTimeout(() => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        world.update();
+        world.step();
         paint();
         requestAnimationFrame(update(t));
       }, 1000 / FPS);
